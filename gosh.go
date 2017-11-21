742b85d3159707e0ebd0a99c5e2afa94be19a6d2
Karl McGuire <karl@karlmcguire.com>
2017-11-20 19:45:42 -0500

    documentation

--- gosh.go
+++ gosh.go
@@ -1,3 +1,5 @@
+// Package gosh provides session handling with automatic timeouts and key-value
+// storage.
 package gosh
 
 import (
@@ -7,8 +9,16 @@ import (
 )
 
 var (
-	ErrAlreadyExists  = errors.New("session with that iden already exists")
-	ErrDoesntExist    = errors.New("no session with that iden exists")
+	// ErrAlreadyExists is thrown when attempting to create/add a new session
+	// and a session with that identifier already exists in the Room.
+	ErrAlreadyExists = errors.New("session with that iden already exists")
+
+	// ErrDoesntExist is thrown when attempting to access a session, but a
+	// session with that identifier doesn't exist in the Room.
+	ErrDoesntExist = errors.New("no session with that iden exists")
+
+	// ErrKeyDoesntExist is thrown when attempting to access a key-value pair
+	// inside of a session, but the key doesn't exist inside that session.
 	ErrKeyDoesntExist = errors.New("that key wasn't found in the session")
 )
 
@@ -30,6 +40,7 @@ func (d *dispatcher) watch(iden string, ping chan struct{}, kill chan string) {
 	}
 }
 
+// Room holds multiple sessions.
 type Room struct {
 	mutex sync.Mutex
 
@@ -39,11 +50,14 @@ type Room struct {
 	killer     chan string
 }
 
-func NewRoom(live time.Duration) *Room {
+// NewRoom returns an empty Room. The lifetime param specifies how long each
+// session inside the Room will live without activity. After the lifetime has
+// expired, the session is automatically deleted from the Room.
+func NewRoom(lifetime time.Duration) *Room {
 	room := &Room{
 		sessions:   make(map[string]map[string]string, 0),
 		watchers:   make(map[string]chan struct{}),
-		dispatcher: &dispatcher{live},
+		dispatcher: &dispatcher{lifetime},
 		killer:     make(chan string, 0),
 	}
 
@@ -64,6 +78,24 @@ func (r *Room) killWatch() {
 	}
 }
 
+func (r *Room) accessCheck(iden, key string) error {
+	if _, ok := r.sessions[iden]; !ok {
+		return ErrDoesntExist
+	}
+	if _, ok := r.watchers[iden]; !ok {
+		return ErrDoesntExist
+	}
+	if key != "" {
+		if _, ok := r.sessions[iden][key]; !ok {
+			return ErrKeyDoesntExist
+		}
+	}
+	return nil
+}
+
+// Add creates a new session identified by the iden param.
+//
+// Add returns an error if a session with that iden already exists.
 func (r *Room) Add(iden string) error {
 	r.mutex.Lock()
 	defer r.mutex.Unlock()
@@ -83,21 +115,12 @@ func (r *Room) Add(iden string) error {
 	return nil
 }
 
-func (r *Room) accessCheck(iden, key string) error {
-	if _, ok := r.sessions[iden]; !ok {
-		return ErrDoesntExist
-	}
-	if _, ok := r.watchers[iden]; !ok {
-		return ErrDoesntExist
-	}
-	if key != "" {
-		if _, ok := r.sessions[iden][key]; !ok {
-			return ErrKeyDoesntExist
-		}
-	}
-	return nil
-}
-
+// Get is for getting session values. The session is identified by the iden
+// parameter. The key parameter is used to find the key-value pair, with the
+// value being returned (if found).
+//
+// Get returns an error if the session doesn't exist or a value doesn't exist
+// for the specified key.
 func (r *Room) Get(iden, key string) (string, error) {
 	r.mutex.Lock()
 	defer r.mutex.Unlock()
@@ -111,6 +134,11 @@ func (r *Room) Get(iden, key string) (string, error) {
 	return r.sessions[iden][key], nil
 }
 
+// Set is for setting session key-value pairs. The session is identified by the
+// iden parameter. The key-value pair is specified by the key and value
+// parameters.
+//
+// Set returns an error if the session doesn't exist.
 func (r *Room) Set(iden, key, value string) error {
 	r.mutex.Lock()
 	defer r.mutex.Unlock()
@@ -125,6 +153,8 @@ func (r *Room) Set(iden, key, value string) error {
 	return nil
 }
 
+// Del deletes the session specified by the iden parameter. It returns an error
+// if the session doesn't exist.
 func (r *Room) Del(iden string) error {
 	r.mutex.Lock()
 	defer r.mutex.Unlock()
